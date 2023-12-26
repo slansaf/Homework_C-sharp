8f471003c3ef04028b3023ad89a0c61c4ab3cd0b
SlanSaf <slansaf@yandex.ru>
2023-12-26 21:11:06 +0300

    Completed the third task

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 98;
+
+if ( number >= 10 && number < 100){
+	int firstNumber = number / 10;
+	int secondNumber = number % 10;
+	if (firstNumber > secondNumber % 10){
+		Console.WriteLine($"Наибольшая цифра числа {firstNumber}");
+	}
+	else {
+		Console.WriteLine($"Наибольшая цифра числа {secondNumber}");
+	}
+}
+
