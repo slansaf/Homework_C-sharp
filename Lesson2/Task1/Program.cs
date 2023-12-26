5f1863161fed3b4658e6003d1c1614d90ec63081
SlanSaf <slansaf@yandex.ru>
2023-12-26 19:48:48 +0300

    Add two folder Task1 and Task2. Completed the first task

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = 161;
+if (number % 7 == 0 && number % 23 == 0){
+	Console.WriteLine($"Чило {number} кратно 7 и 23");
+}
+else{
+	Console.WriteLine($"Чило {number} не кратно 7 и 23");
+}
